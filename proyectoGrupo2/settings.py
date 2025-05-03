d9f7b229d93659993557bce9058b962e96dda9fe
matikasssssss <matias.farias@ug.uchile.cl>
2025-05-03 04:23:04 -0400

    Loggin básico, falta arreglar unas pequeños detalles

--- proyectoGrupo2/settings.py
+++ proyectoGrupo2/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'proyectoGrupo2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'todoapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'  
+LOGOUT_REDIRECT_URL = '/login/'  
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def inicio(request):
+    return render(request, 'inicio.html')
